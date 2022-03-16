e9ae6bcfe48aeaae9488132fc6329c56aeaf2e70
JonnyRiver <jonnyriver6@gmail.com>
2022-03-16 10:00:00 -0400

    comment.js

--- models/pizza.js
+++ models/pizza.js
@@ -1,23 +1,30 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model } = require("mongoose");
 
 const PizzaSchema = new Schema({
   pizzaName: {
-    type: String
+    type: String,
   },
   createdBy: {
-    type: String
+    type: String,
   },
   createdAt: {
     type: Date,
-    default: Date.now
+    default: Date.now,
   },
   size: {
     type: String,
-    default: 'Large'
+    default: "Large",
   },
-  toppings: []
+  toppings: [],
+
+  comments: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Comment",
+    },
+  ],
 });
 
-const Pizza = model('Pizza', PizzaSchema);
+const Pizza = model("Pizza", PizzaSchema);
 
 module.exports = Pizza;
